f1d0eaf80132f4bd91beb30e1ebaaa3dfe7ee620
D. Syafa'atul Anbiya <dikisyafaatul@gmail.com>
2020-11-05 09:32:26 +0700

    add button login

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
 <body>
     <form action="">
         <input type="text" name="username" placeholder="Username"> <input type="password" name="password" placeholder="Password">
+        <input type="submit" value="Login">
     </form>
 </body>
 </html>
