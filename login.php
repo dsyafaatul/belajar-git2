1b3a37ac7887c7deb14db680b48695d75267efe6
D. Syafa'atul Anbiya <dikisyafaatul@gmail.com>
2020-11-12 07:58:37 +0700

    Add Heading at login.php

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
     <title>Login</title>
 </head>
 <body>
+    <h1>Silahkan login terlebih dahulu untuk masuk ke dashboard</h1>
     <form action="">
         <input type="text" name="username" placeholder="Username">
         <input type="password" name="password" placeholder="Password">
