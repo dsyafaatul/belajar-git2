74cf168869136dccc49203dd1ef05aaf25bf4be5
D. Syafa'atul Anbiya <dikisyafaatul@gmail.com>
2020-11-12 18:18:15 +0700

    coba commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
 </head>
 <body>
     <h1>Selamat datang, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam sapiente culpa vel ea, impedit corrupti repellendus odit nihil dicta quis magnam cupiditate praesentium aliquam recusandae ipsum ab nulla possimus! Sed!</h1>
